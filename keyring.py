d5d9737890b3a0edf16de1b4d453abdc077837a2
Gabrel Leopoldino <gabrielperes97@gmail.com>
2018-09-14 15:10:24 -0300

    Featura: generate passwords

--- keyring.py
+++ keyring.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from cryptography.fernet import InvalidToken
 from security import DefaultCryptography
 from storage import SecureStorage
+from utils import generateKey
 import os
 import sys
 import pyperclip
@@ -134,6 +135,24 @@ def remove(args):
     db.remove(doc_ids=[result[0].doc_id])
     print("Password ", args.label, " removed successfully")
 
+def generate(args):
+    print("Generating password")
+    password = generateKey(length=args.length, uppers=not args.no_uppercase, lowers=not args.no_lowercase, punctuation=not args.no_punctuation, digits= not args.no_digits, except_chars=args.except_chars)
+    if (args.save):
+        db = login(args.file)
+        query = Query()
+        if (len(db.search(query.label == args.save)) > 0):
+            print("This label already in use")
+            exit(-1)
+        db.insert({'label': args.save, 'password': password, 'created_date': datetime.now().isoformat(), 'last_access': None, 'last_update': None})
+        print("Password has been saved with the label ", args.save)
+    if (args.to_clipboard):
+        pyperclip.copy(password)
+        print("Password copied to clipboard")
+    else:
+        print("The generated password is: ", password)
+
+
     
 # Menu 
 parser = ArgumentParser()
@@ -149,7 +168,7 @@ add_parser.set_defaults(func=add)
 
 get_parser = subparsers.add_parser("get", help="get a password")
 get_parser.add_argument("label", help="label for the password")
-get_parser.add_argument("-c", "--to-clipboard", action="store_true" ,help="Copy the password to clipboard and don't show it")
+get_parser.add_argument("-c", "--to-clipboard", action="store_true", help="Copy the password to clipboard and don't show it")
 get_parser.set_defaults(func=get)
 
 update_parser = subparsers.add_parser("update", help="update a password")
@@ -159,6 +178,17 @@ update_parser.set_defaults(func=update)
 remove_parser = subparsers.add_parser("remove", help="Remove a password")
 remove_parser.add_argument("label", help="label for the password to remove")
 remove_parser.set_defaults(func=remove)
+
+generate_parser = subparsers.add_parser("generate", help="generate a password")
+generate_parser.add_argument("-l", "--length", metavar="length", type=int, default="12", help="The length for the generated password [default=12]")
+generate_parser.add_argument("-u", "--no-uppercase", action="store_true", help="Don't use uppercase chars in the password")
+generate_parser.add_argument("-ll", "--no-lowercase", action="store_true", help="Don't use lowercase chars in the password")
+generate_parser.add_argument("-d", "--no-digits", action="store_true", help="Don't use digits in the password")
+generate_parser.add_argument("-p", "--no-punctuation", action="store_true", help="Don't use punctuation chars in the password")
+generate_parser.add_argument("-e", "--except-chars", metavar="except_chars", default="", help="Don't use these chars in the password")
+generate_parser.add_argument("-s", "--save", metavar="label", help="Save the generate password using this label")
+generate_parser.add_argument("-c", "--to-clipboard", action="store_true", help="Copy the password to clipboard and don't show it")
+generate_parser.set_defaults(func=generate)
 
 
 if __name__ == "__main__":
