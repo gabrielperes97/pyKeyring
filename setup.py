8000b51bc60c6c0335804f6cd856819cfa2782c0
Gabrel Leopoldino <gabrielperes97@gmail.com>
2018-09-16 14:13:14 -0300

    Version updated

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ entry_points = None
 setup(
     name='pyKeyring',
     py_modules=['keyring', 'security', 'storage', 'utils'],
-    version="0.1",
+    version="0.2",
     description='A simple and secure tool to store passwords',
     long_description=read('README.rst'),
     url='https://github.com/gabrielperes97/pyKeyring',
